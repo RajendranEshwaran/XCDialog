6876e34dd5ee9e0f9e9f803593550d815dcb9999
Rajendran Eshwaran <rajendran.eshwaran@amerigas.com>
2020-02-24 20:28:26 -0500

    Initial Commit

---
+++ XCDialog/XCDialog.h
@@ -0,0 +1,19 @@
+//
+//  XCDialog.h
+//  XCDialog
+//
+//  Created by gomathi saminathan on 2/24/20.
+//  Copyright © 2020 Rajendran Eshwaran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCDialog.
+FOUNDATION_EXPORT double XCDialogVersionNumber;
+
+//! Project version string for XCDialog.
+FOUNDATION_EXPORT const unsigned char XCDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCDialog/PublicHeader.h>
+
+
