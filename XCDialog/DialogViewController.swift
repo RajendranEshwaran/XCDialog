a58b1cf6e592a4db63cbbc59f3b96f4c8cf4eb92
GomathiSaminathan <gomathi.saminathan@outlook.com>
2020-03-05 21:23:56 -0500

    Implementing design content for showing custom dialog for the user.(Framework demo)

---
+++ XCDialog/DialogViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DialogViewController.swift
+//  XCDialog
+//
+//  Created by gomathi saminathan on 3/5/20.
+//  Copyright © 2020 Rajendran Eshwaran. All rights reserved.
+//
+
+import UIKit
+
+class DialogViewController: UIViewController {
+
+    @IBOutlet var dialogImage:UIImageView!
+    @IBOutlet var dialogMsg: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
